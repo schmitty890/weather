b02bd71c42a73c5926fd6a19688517229408d48d
Jason Schmitt <schmitty890@gmail.com>
2019-06-15 14:09:33 -0400

    accepting zip code values

--- client/src/components/SearchNav/SearchNav.js
+++ client/src/components/SearchNav/SearchNav.js
@@ -4,17 +4,12 @@ import { Navbar, Form, FormControl, Button } from 'react-bootstrap';
 
 const Hero = props => {
     console.log(props);
+    let input;
     return (
         <Navbar bg="dark" variant="dark">
-        {/* <Navbar.Brand href="#home">Weather App</Navbar.Brand> */}
-        {/* <Nav className="mr-auto"> */}
-        {/* <Nav.Link href="#home">Home</Nav.Link> */}
-        {/* <Nav.Link href="#features">Features</Nav.Link>
-        <Nav.Link href="#pricing">Pricing</Nav.Link> */}
-        {/* </Nav> */}
         <Form inline>
-            <FormControl inputRef={ref => { this.myInput = ref; }} type="text" placeholder="zipcode" className="mr-sm-2" />
-            <Button variant="outline-info" onClick={(event) => props.action(event, '27518')}>Search by zipcode!</Button>
+            <FormControl ref={node => input = node} type="text" placeholder="zipcode" className="mr-sm-2" />
+            <Button variant="outline-info" onClick={(event) => props.action(event, input.value)}>Search by zipcode!</Button>
         </Form>
         </Navbar>
     )
