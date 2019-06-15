da8ad1e2ec6ba2df1658bc20342feb03a5211b0c
Jason Schmitt <schmitty890@gmail.com>
2019-06-15 15:27:00 -0400

    displaying props passed to hero component

--- client/src/components/Hero/Hero.js
+++ client/src/components/Hero/Hero.js
@@ -6,7 +6,17 @@ const Hero = props => {
     console.log(props);
     return (
         <Row>
-            <Col sm={12} className={styles.hero}>{props.city}</Col>
+            <Col sm={12} className={styles.hero}>
+                <div>{props.city}</div>
+                <div>{props.cloudiness}</div>
+                <div>{props.description}</div>
+                <div>{props.humidity}</div>
+                <div>{props.pressure}</div>
+                <div>{props.sunrise}</div>
+                <div>{props.sunset}</div>
+                <div>{props.temperature}</div>
+                <div>{props.windSpeed}</div>
+            </Col>
         </Row>
     )
 }
